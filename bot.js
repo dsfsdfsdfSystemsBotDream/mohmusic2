ef9a89c8942e5de8790d215c3b0e9afafc92ae0a
dsfsdfsdfSystemsBotDream <45539387+dsfsdfsdfSystemsBotDream@users.noreply.github.com>
2019-01-06 21:15:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,8 +20,8 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "4";
-var adminprefix = '4'
+const prefix = "2";
+var adminprefix = '2'
 /////////////////////////
 ////////////////////////
 
@@ -315,7 +315,7 @@ function play(guild, song) {
 }
 })
 
-const developers = ["496585065673916417","id"]
+const developers = ["422461839683878912","352801370577043456","518872353615380497"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
